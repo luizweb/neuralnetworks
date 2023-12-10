888b11a2e8db73dd3fc1a5d169900d0bf18a167c
Luiz Simoes <luizgsantonio@gmail.com>
2023-12-09 21:10:04 -0300

    update main - part 6

--- main.py
+++ main.py
@@ -1,60 +1,29 @@
+# Softmax Activation
 
-# ACTIVATION FUNCTION
+import math
 
-# Step Function
-# y = 1  x > 0
-# y = 0  x <= 0
+layer_outputs = [4.8, 1.21, 2.385]
 
-# Sigmoid function
+# número Euler
+# E = 2.718281828459045
+E = math.e
 
-# ReLU function - Rectified Linear Unit
-'''
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
+# exponentials
+exp_values = []
 
+for output in layer_outputs:
+    exp_values.append(E**output)
 
-for i in inputs:
-    output.append(max(0, i))
+print(exp_values)
 
-print(output)
-'''
-'''
-for i in inputs:
-    if i > 0:
-        output.append(i)
-    elif i <= 0:
-        output.append(0)
+# normalization
 
-print(output)
-'''
+norm_base = sum(exp_values)
+norm_values = []
 
-import numpy as np
-import nnfs
-from nnfs.datasets import spiral_data
+for values in exp_values:
+    norm_values.append( values / norm_base )
 
-nnfs.init()
+print(norm_values)
 
-X, y = spiral_data(100, 3)
-
-
-class Layer_Dense():
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons) # valores mais ou menos entre -1 e 1
-        self.biases = np.zeros((1, n_neurons)) # bias iniciado com zeros
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases # não precisa mais do transpose
-
-
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-
-layer1 = Layer_Dense(2,5)
-activation1 = Activation_ReLU()
-
-layer1.forward(X)
-#print(layer1.output) # aqui aparecem alguns números negativos
-activation1.forward(layer1.output)
-print(activation1.output) # os numeros negativos viraram zeros
+# parei em 11:45    
